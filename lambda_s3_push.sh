caa89d638661a40a5530413a58b925d27b15f2d8
rickgarcia <rick.2g@gmail.com>
2025-02-20 13:09:42 -0600

    updating bucket

--- lambda_s3_push.sh
+++ lambda_s3_push.sh
@@ -1,6 +1,6 @@
 aws lambda publish-layer-version \
     --layer-name pytorch-transformers \
     --description "PyTorch and Transformers libraries for ML inference" \
-    --content S3Bucket=lambda-layer-test,S3Key=pytorch-transformers-layer.zip \
+    --content S3Bucket=sbx-lambda-layer-test,S3Key=pytorch-transformers-layer.zip \
     --compatible-runtimes python3.9 \
     --compatible-architectures x86_64
