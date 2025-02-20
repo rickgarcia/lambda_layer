91edf725efa4b1c60a71f481e9125a5cfbc67abc
rickgarcia <rick.2g@gmail.com>
2025-02-20 12:31:12 -0600

    updated files

--- build-layer.sh
+++ build-layer.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# Create a Docker container to build the packages
-docker run --rm -v $(pwd):/var/task public.ecr.aws/sam/build-python3.9:latest \
-    pip install -r requirements.txt -t python/
+echo "Starting layer build process..."
+echo "Current directory: $(pwd)"
+echo "Contents of current directory:"
+ls -la
 
-# Create the layer zip file
+echo "Creating Docker container to build packages..."
+docker run --rm -v $(pwd):/var/task public.ecr.aws/sam/build-python3.9:latest \
+    pip install -r requirements.txt -t python/ --verbose
+
+echo "Contents of python directory after install:"
+ls -la python/
+
+echo "Creating layer zip file..."
 zip -r pytorch-transformers-layer.zip python/
+
+echo "Build complete. Layer size:"
+ls -lh pytorch-transformers-layer.zip
